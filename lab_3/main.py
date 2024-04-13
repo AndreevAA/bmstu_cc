34d6fcac9550d8653e3f6a1523b33dfebee4f073
Andreev Alexander <andreev.business.mail@gmail.com>
2024-04-13 06:34:34 +0300

    added main root function with five tests

---
+++ lab_3/main.py
@@ -0,0 +1,34 @@
+from parser import Parser
+
+tests = [
+    '{ a := 3 ; aaa := p ; { a := p ; x := p and not p } }', 
+    '{ a := ( abs p and ( 1 ** 25 ) ) }', 
+    # '{ a? := 1 and - ( + p and 1 ** 2 ** 455 ) }', 
+    '{ c := not ( p mod 5 / ( 2 + 2 ) ) }', 
+    '{ c := abs ( - p + 2 & 4 mod 5 / ( 2 ) ) }', 
+    '{ ll := ( p and - 22 ) }'
+]
+
+def main(input_string):
+    input_string_split = list(input_string.strip().split())
+    print()
+    print('Tokens')
+    print(input_string_split)
+    parser = Parser(input_string_split)
+
+
+if __name__ == "__main__":
+    
+    for _ in tests:
+        main(_)
+
+    # input = '{ a := 3 ; aaa := p ; { a := p ; x := p and not p } }'
+    # input = '{ a := ( abs p and ( 1 ** 25 ) ) }'
+    # input = '{ a? := 1 and - ( + p and 1 ** 2 ** 455 ) }'
+    # input = '{ c := not ( p mod 5 / ( 2 + 2 ) ) }'
+    # input = '{ c := abs ( - p + 2 & 4 mod 5 / ( 2 ) ) }'
+    #input = '{ ll := ( p and - 22 ) }'
+    #input = list(input.strip().split())
+    # print('Tokens')
+    # print(input)
+    # parser = Parser(input)
