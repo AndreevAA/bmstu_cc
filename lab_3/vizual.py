eee5326ef5ed7a87a61d1a4d781858a7f7f39068
Andreev Alexander <andreev.business.mail@gmail.com>
2024-04-20 09:59:02 +0300

    updated nodes creation with template

--- lab_3/vizual.py
+++ lab_3/vizual.py
@@ -25,13 +25,15 @@ class Parser:
     def __get_new_node_id(self):
         self.node_cnt += 1
         return self.node_cnt
-        
-    def __get_start_node(self):
-        return Node(parent_id = None, id=self.__get_new_node_id(), value='program')
+    
+    def __create_new_node(self, parent_id, id, value):
+        tmp_node = Node(parent_id = parent_id, id=id, value=value)
+        self.node_lst.append(tmp_node)
 
+        return tmp_node
+        
     def __program(self):
-        start_node = self.__get_start_node()
-        self.node_lst.append(start_node)
+        start_node = self.__create_new_node(parent_id = None, id=self.__get_new_node_id(), value='program')
         
         if self.__block(start_node):
             return True
@@ -45,30 +47,25 @@ class Parser:
     def __get_current_token(self):
         if self.i < len(self.input):
             return self.input[self.i]
-        
-    def create_new_node(self, parent_id, id, value):
-        tmp_node = Node(parent_id = parent_id, id=id, value=value)
-        self.node_lst.append(tmp_node)
-        return tmp_node
 
     def __block(self, parent_node):
-        block_node = self.create_new_node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='block')
+        block_node = self.__create_new_node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='block')
 
         if self.__get_current_token() == '{':
-            left_brack_node = Node(parent_id = block_node.id, id=self.__get_new_node_id(), value='{')
-            self.node_lst.append(left_brack_node)
+            left_brack_node = self.__create_new_node(parent_id = block_node.id, id=self.__get_new_node_id(), value='{')
+            #self.node_lst.append(left_brack_node)
             self.i += 1
 
             if self.__get_current_token() == '}':
-                right_brack_node = Node(parent_id = block_node.id, id=self.__get_new_node_id(), value='}')
-                self.node_lst.append(right_brack_node)
+                right_brack_node = self.__create_new_node(parent_id = block_node.id, id=self.__get_new_node_id(), value='}')
+                #self.node_lst.append(right_brack_node)
                 print("block", self.__get_current_token())
                 return True
 
             elif self.__operators_list(block_node):
                 if self.__get_current_token() == '}':
-                    right_brack_node = Node(parent_id = block_node.id, id=self.__get_new_node_id(), value='}')
-                    self.node_lst.append(right_brack_node)
+                    right_brack_node = self.__create_new_node(parent_id = block_node.id, id=self.__get_new_node_id(), value='}')
+                    #self.node_lst.append(right_brack_node)
                     print("block", self.__get_current_token())
                     return True
                 else:
@@ -76,8 +73,8 @@ class Parser:
             self.__error()
 
     def __operators_list(self, parent_node):
-        __operators_list_node = Node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='operators_list')
-        self.node_lst.append(__operators_list_node)
+        __operators_list_node = self.__create_new_node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='operators_list')
+        #self.node_lst.append(__operators_list_node)
         
         if self.__operator(__operators_list_node):
             print("operator", self.__get_current_token())
@@ -90,17 +87,17 @@ class Parser:
         self.__error()
 
     def __operator(self, parent_node):
-        operator_node = Node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='operator_node')
-        self.node_lst.append(operator_node)
+        operator_node = self.__create_new_node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='operator_node')
+        #self.node_lst.append(operator_node)
 
         if self.__get_current_token().isalpha():
-            operator_node_value = Node(parent_id = operator_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
-            self.node_lst.append(operator_node_value)
+            operator_node_value = self.__create_new_node(parent_id = operator_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
+            #self.node_lst.append(operator_node_value)
             print("operator", self.__get_current_token())
             self.i += 1
             if self.__get_current_token() == ':=':
-                operator_node_value = Node(parent_id = operator_node.id, id=self.__get_new_node_id(), value=':=')
-                self.node_lst.append(operator_node_value)
+                operator_node_value = self.__create_new_node(parent_id = operator_node.id, id=self.__get_new_node_id(), value=':=')
+                #self.node_lst.append(operator_node_value)
                 print("operator", self.__get_current_token())
                 self.i += 1
                 if self.__expression(operator_node):
@@ -113,22 +110,22 @@ class Parser:
             return self.__error()
 
     def __factor(self, parent_node):
-        factor_node = Node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='factor_node')
-        self.node_lst.append(factor_node)
+        factor_node = self.__create_new_node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='factor_node')
+        #self.node_lst.append(factor_node)
 
         print("factor", self.__get_current_token())
 
         if self.__get_current_token() in ('abs', 'not'):
-            factor_node_value = Node(parent_id = factor_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
-            self.node_lst.append(factor_node_value)
+            factor_node_value = self.__create_new_node(parent_id = factor_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
+            #self.node_lst.append(factor_node_value)
             self.i += 1
             if self.__primary(factor_node):
                 return True
             self.__error()
         if self.__primary(factor_node):
             while self.__get_current_token() == '**':
-                factor_node_value = Node(parent_id = factor_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
-                self.node_lst.append(factor_node_value)
+                factor_node_value = self.__create_new_node(parent_id = factor_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
+                #self.node_lst.append(factor_node_value)
                 self.i += 1
                 if self.__primary(factor_node):
                     continue
@@ -136,46 +133,46 @@ class Parser:
                     self.__error()
             return True
         else:
-            factor_node_value = Node(parent_id = factor_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
-            self.node_lst.append(factor_node_value)
+            factor_node_value = self.__create_new_node(parent_id = factor_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
+            #self.node_lst.append(factor_node_value)
             self.i += 1
             if self.__primary(factor_node):
                 return True
             self.__error()
 
     def __primary(self, parent_node):
-        primary_node = Node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='primary_node')
-        self.node_lst.append(primary_node)
+        primary_node = self.__create_new_node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='primary_node')
+        #self.node_lst.append(primary_node)
 
         if self.__get_current_token() == 'p' or self.__get_current_token().isdigit():
-            primary_node_value = Node(parent_id = primary_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
-            self.node_lst.append(primary_node_value)
+            primary_node_value = self.__create_new_node(parent_id = primary_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
+            #self.node_lst.append(primary_node_value)
             self.i += 1
             
             print("primary", self.__get_current_token())
             return True
         if self.__get_current_token() == '(':
-            primary_node_value = Node(parent_id = primary_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
-            self.node_lst.append(primary_node_value)
+            primary_node_value = self.__create_new_node(parent_id = primary_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
+            #self.node_lst.append(primary_node_value)
             self.i += 1
             if self.__expression(primary_node):
                 print("expression", self.__get_current_token())
                 if self.__get_current_token() == ')':
-                    primary_node_value = Node(parent_id = primary_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
-                    self.node_lst.append(primary_node_value)
+                    primary_node_value = self.__create_new_node(parent_id = primary_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
+                    #self.node_lst.append(primary_node_value)
                     self.i += 1
                     return True
             self.__error()
 
     def __expression(self, parent_node):
-        expression_node = Node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='expression_node')
-        self.node_lst.append(expression_node)
+        expression_node = self.__create_new_node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='expression_node')
+        #self.node_lst.append(expression_node)
 
         if self.__relation(expression_node):
             print("relation", self.__get_current_token())
             if self.__get_current_token() in ('and', 'or', 'xor'):
-                expression_node_value = Node(parent_id = expression_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
-                self.node_lst.append(expression_node_value)
+                expression_node_value = self.__create_new_node(parent_id = expression_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
+                #self.node_lst.append(expression_node_value)
                 self.i += 1
                 print("logic operation", self.__get_current_token())
 
@@ -188,13 +185,13 @@ class Parser:
             self.__error()
 
     def __relation(self, parent_node):
-        relation_node = Node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='relation_node')
-        self.node_lst.append(relation_node)
+        relation_node = self.__create_new_node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='relation_node')
+        #self.node_lst.append(relation_node)
         
         if self.__simple_expression(relation_node):
             if self.__get_current_token() in ('<', '<=', '==', '/>', '>=', '>'):
-                relation_node_value = Node(parent_id = relation_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
-                self.node_lst.append(relation_node_value)
+                relation_node_value = self.__create_new_node(parent_id = relation_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
+                #self.node_lst.append(relation_node_value)
                 self.i += 1
 
                 if self.__simple_expression(relation_node):
@@ -205,13 +202,13 @@ class Parser:
             self.__error()
 
     def __term(self, parent_node):
-        term_node = Node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='term_node')
-        self.node_lst.append(term_node)
+        term_node = self.__create_new_node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='term_node')
+        #self.node_lst.append(term_node)
 
         if self.__factor(term_node):
             while self.__get_current_token() in ('*', '/', 'mod', 'rem'):
-                term_node_value = Node(parent_id = term_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
-                self.node_lst.append(term_node_value)
+                term_node_value = self.__create_new_node(parent_id = term_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
+                #self.node_lst.append(term_node_value)
                 self.i += 1
 
                 if self.__factor(term_node):
@@ -221,17 +218,17 @@ class Parser:
             return True
 
     def __simple_expression(self, parent_node):
-        simple_expression_node = Node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='simple_expression_node')
-        self.node_lst.append(simple_expression_node)
+        simple_expression_node = self.__create_new_node(parent_id = parent_node.id, id=self.__get_new_node_id(), value='simple_expression_node')
+        #self.node_lst.append(simple_expression_node)
 
         if self.__get_current_token() in ('-', '+'):
-            simple_expression_node_value = Node(parent_id = simple_expression_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
-            self.node_lst.append(simple_expression_node_value)
+            simple_expression_node_value = self.__create_new_node(parent_id = simple_expression_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
+            #self.node_lst.append(simple_expression_node_value)
             self.i += 1
         if self.__term(simple_expression_node):
             while self.__get_current_token() in ('+', '-', '&'):
-                simple_expression_node_value = Node(parent_id = simple_expression_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
-                self.node_lst.append(simple_expression_node_value)
+                simple_expression_node_value = self.__create_new_node(parent_id = simple_expression_node.id, id=self.__get_new_node_id(), value=self.__get_current_token())
+                #self.node_lst.append(simple_expression_node_value)
                 self.i += 1
                 
                 if self.__term(simple_expression_node):
@@ -244,8 +241,8 @@ class Parser:
 
     def __tail(self, parent_node):
         if self.__get_current_token() == ';':
-            tail_node = Node(parent_id = parent_node.id, id=self.__get_new_node_id(), value=';')
-            self.node_lst.append(tail_node)
+            tail_node = self.__create_new_node(parent_id = parent_node.id, id=self.__get_new_node_id(), value=';')
+            #self.node_lst.append(tail_node)
         
             self.i += 1
             if self.__operator(parent_node):
