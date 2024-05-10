828fc416393ee883a096d1f8c736ac2f26a8b6fc
AndreevAA <58621780+AndreevAA@users.noreply.github.com>
2024-05-10 13:52:18 +0300

    updated drawing schemas

--- lab_4/node.py
+++ lab_4/node.py
@@ -1,39 +1,46 @@
+import typing
+
 class Node:
+	name = ""
+	children: list[typing.Type["Node"]]
+	parent_id = -1
+	child_id = -1
+	content = None
 
-    def __init__(self, name = None, state_proxy = False):
-        self.name = name
-        self.children = list()
-        self.content = list()
-       	self.parent_id = -1
-       	self.child_id = -1
+	def __init__(self, name = None, state_proxy = False) -> None:
+		self.state_proxy = state_proxy
+		self.name = name
+		self.children = []
+		self.content = []
+		pass
 
-    def __is_chilren_exist(self):
-    	if self.children == None:
-    		return False
-    	elif len(self.children) == 0:
-    		return False
-    	return True
+	def __is_chilren_exist(self):
+		if self.children == None:
+			return False
+		elif len(self.children) == 0:
+			return False
+		return True
 
-    def __update_connections(self, node):
-		if not self.__is_chilren_exist():
-            self.parent_id = node.parent_id
-        self.child_id = node.child_id
+	def __update_connections(self, node):
+		if not len(self.children):
+			self.parent_id = node.parent_id
+		self.child_id = node.child_id
 
-    def __add_child_proxy(self, node):
-    	for _ in node.children:
+	def __add_child_proxy(self, node):
+		for _ in node.children:
 			self.add_child_node(_)
 
 	def __add_child_narrow(self, node):
-		if node != None:
-			self.children.append(node)
+		self.children.append(node)
 
-    def __update_children(self, node):
-    	if self.state_proxy:
-    		self.__add_child_proxy(node)
-    	else:
-    		self.__add_child_narrow(node)
+	def __update_children(self, node):
+		if node.name in ['<proxy>']:
+			self.__add_child_proxy(node)
+		else:
+			self.__add_child_narrow(node)
 
-    def add_child_node(self, node):
-        self.__update_connections(node)
-        self.__update_children(node)
+	def add_child_node(self, node:typing.Type["Node"]):
+		self.__update_connections(node)
+		self.__update_children(node)
+
 
--- lab_4/tree.py
+++ lab_4/tree.py
@@ -1,32 +1,84 @@
 from graphviz import Digraph
+import html
 
 class Tree:
-
+    """
+    Класс дерева.
+    """
     def __init__(self, root_node):
         self.root_node = root_node
         self.tree = Digraph()
-        self.__add_node(root_node)
+        self.tree.node_attr["shape"] = "plaintext"
+        # self.tree.node_attr["color"] = "white"
+        # self.tree.node_attr["style"] = "filled"
+        # self.tree.node_attr["fontcolor"] = "black"
+        # self.tree.graph_attr["bgcolor"] = "white"
+        # self.tree.edge_attr["color"] = "#9370DB"
+        # self.tree.edge_attr["style"] = "dashed"
+        # self.tree.body = [
+        #     'rankdir=LR',
+        #     'label="\N{sparkles} Welcome to the Magical Graph \N{sparkles}"',
+        #     'fontsize=20']
+        self._add_node(root_node, parent_id="-1", node_id="root")
 
-    def __add_node(self, node, parent_id = "-1", node_id = "root"):
-
-        node_name = "node_name: " + root_node.name
-        node_content = "node_content: "
-
-        for _ in node.content:
-            node_content += str(_)
-
-        self.tree.node(node_id, 
-            node_name + "\n" +
-            node_content + )
-
+    def _create_parent_edge(self, parent_id, node_id):
+        """
+        Создает ребро между родительским и дочерним узлами в графе.
+        """
         if parent_id != "-1":
-            tree.edge(parent_id, node_id)
+            self.tree.edge(parent_id, node_id)
+
+    def _add_child_nodes(self, node, parent_id, node_id):
+        """
+        Рекурсивно добавляет дочерние узлы к текущему узлу в графе.
+        """
+        for index, child in enumerate(node.children):
+            child_node_id = f"{node_id}.{index}"
+            self._add_node(child, parent_id=node_id, node_id=child_node_id)
+
+    def _add_node_table(self, node_id, node_name, node_content_str):
+        self.tree.node(node_id, f"""<
+        <table border="0" cellborder="1" cellspacing="0" cellpadding="4">
+          <tr>
+            <td colspan="3">{html.escape(node_name)}</td>
+          </tr>
+          <tr>
+            <td colspan="3">{html.escape(node_content_str)}</td>
+          </tr>
+        </table>>""")
+
+
+    def _add_node_name(self, node_id, node_name):
+        self.tree.node(node_id, 
+            f'''<
+            <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0" CELLPADDING="4">
+              <TR>
+                <TD COLSPAN="3">{html.escape(node_name)}</TD>
+              </TR>
+            </TABLE>>''')
+
+    def _add_node(self, node, parent_id="-1", node_id="root"):
+        """
+        Добавляет узел к графу и вызывает методы для создания ребра с родительским узлом
+        и добавления дочерних узлов.
+        """
+        node_name = f"Node: {node.name}"
+        node_content = "Content: "
+        for item in node.content:
+            node_content += str(item)
+
+        node_content_str = f'{node_content}' if node.content else ''
+
+        if node_content_str != '':
+            self._add_node_table(node_id, node_name, node_content_str)
+        else:
+            self._add_node_name(node_id, node_name)
+        self._create_parent_edge(parent_id, node_id)
+        self._add_child_nodes(node, parent_id, node_id)
+
 
     def render(self):
-        self.__get_all_tree()
+        """
+        Отображает дерево в формате PNG.
+        """
         self.tree.render("parse_tree", format="png", view=True)
-
-    def __get_all_tree(self):
-        for _, child in enumerate(self.root_node.children):
-            tmp_node_id = str(node_id + "." + str(_))
-            self.__add_node(child, parent_id = node_id, node_id = tmp_node_id)
